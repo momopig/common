77e5fe308f055196279de4d53e0f6e3c225caa66
蔡剑涛 <caijiantao@haizhi.com>
2017-03-24 15:44:00 +0800

    增加值转义函数,防止js注入

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
 /**
  * Created by Jamter on 17/2/7.
  */
+/**
+ * @desc 对敏感字符逗号、尖括号进行转义,防止js注入
+ * @param v {string} <script>alert("1")</script> 
+ * */
+function escapeValue(v) {
+    var  entry = { "'": "&apos;", '"': '&quot;', '<': '&lt;', '>': '&gt;' };
+    v = v.replace(/(['")-><&\\\/\.])/g, function ($0) { return entry[$0] || $0; });
+    return v;
+}
